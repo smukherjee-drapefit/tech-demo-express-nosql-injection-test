3af5f6f347a49b660bebbd16125a1cae3f2ca317
Secret Guard <gsecret054@gmail.com>
2023-01-31 07:09:03 +0100

    add code snippet for preventing nosql-injection

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
+// Prevent NoSQL injection
+const mongoSanitize = require('express-mongo-sanitize');
+
 const routes = require('./routes/index');
 
 const app = express();
-
 app.use(express.json());
-
 app.use(routes);
+
+// Prevent NoSQL injection
+app.use(
+  mongoSanitize({
+    onSanitize: ({ req, key }) => {
+      // Throw an error
+      // Catch with express error handler
+    }
+  })
+);
 
 mongoose
   .connect(process.env.MONGODB_URI)
